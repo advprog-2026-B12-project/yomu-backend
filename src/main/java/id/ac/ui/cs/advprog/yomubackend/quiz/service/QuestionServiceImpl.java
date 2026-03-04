de7333c2eb9870d0b4188c2beb9afa1ef73da4b0
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2026-03-05 02:34:40 +0700

    feat/quiz: create quiz services

---
+++ src/main/java/id/ac/ui/cs/advprog/yomubackend/quiz/service/QuestionServiceImpl.java
@@ -0,0 +1,45 @@
+package id.ac.ui.cs.advprog.yomubackend.quiz.service;
+
+import id.ac.ui.cs.advprog.yomubackend.quiz.model.Question;
+import id.ac.ui.cs.advprog.yomubackend.quiz.model.Reading;
+import id.ac.ui.cs.advprog.yomubackend.quiz.repository.QuestionRepository;
+import id.ac.ui.cs.advprog.yomubackend.quiz.repository.ReadingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class QuestionServiceImpl implements QuestionService {
+
+    private final QuestionRepository questionRepository;
+    private final ReadingRepository readingRepository;
+
+    public QuestionServiceImpl(QuestionRepository questionRepository,
+                               ReadingRepository readingRepository) {
+        this.questionRepository = questionRepository;
+        this.readingRepository = readingRepository;
+    }
+
+    @Override
+    public Question create(UUID readingId, Question question) {
+        Reading reading = readingRepository.findById(readingId)
+                .orElseThrow(() -> new IllegalArgumentException("Reading not found"));
+
+        question.setReading(reading);
+        return questionRepository.save(question);
+    }
+
+    @Override
+    public List<Question> findByReading(UUID readingId) {
+        return questionRepository.findAll()
+                .stream()
+                .filter(q -> q.getReading().getId().equals(readingId))
+                .toList();
+    }
+
+    @Override
+    public void delete(UUID questionId) {
+        questionRepository.deleteById(questionId);
+    }
+}
