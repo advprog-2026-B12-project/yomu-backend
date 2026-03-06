96b328ed8d913df4b00293dee6ac4ab4ec1a0369
inoperatingsystem <julius.albert@cs.ui.ac.id>
2026-03-06 15:17:51 +0700

    chore: add AchievementEvent constants as source of truth for event types

---
+++ src/main/java/id/ac/ui/cs/advprog/yomubackend/achievements/constant/AchievementEvent.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.yomubackend.achievements.constant;
+
+public final class AchievementEvent {
+    private AchievementEvent() {}
+
+    public static final String READING_COMPLETED = "READING_COMPLETED";
+    public static final String QUIZ_FINISHED = "QUIZ_FINISHED";
+    public static final String PERFECT_QUIZ_SCORE = "PERFECT_QUIZ_SCORE";
+    public static final String CLAN_PROMOTION = "CLAN_PROMOTION";
+}
