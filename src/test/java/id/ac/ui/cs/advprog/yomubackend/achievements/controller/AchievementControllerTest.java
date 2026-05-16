aaee2632b9ec2eccd65a100c4ae7122f851194b4
inoperatingsystem <julius.albert@ui.ac.id>
2026-05-16 15:26:17 +0700

    test(achievements): fix missing import

--- src/test/java/id/ac/ui/cs/advprog/yomubackend/achievements/controller/AchievementControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/yomubackend/achievements/controller/AchievementControllerTest.java
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
